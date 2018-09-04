7add5cb91a022284ba25f5221b703ca88f4d3ce6
Matthew Rideout <boon4376@gmail.com>
2018-09-04 18:28:09 -0400

    urlset data is added to the array of all urls

--- index.js
+++ index.js
@@ -4,27 +4,68 @@ const axios = require('axios');
 
 
 let sitemapArray = [
-    "https://www.delish.com/sitemap_index.xml/",
-    "https://www.allrecipes.com/gsindex.xml",
-    "https://www.thepaleomom.com/sitemap_index.xml",
-    "https://www.strictchef.com/sitemap.xml"
+    "https://www.delish.com/sitemap_index.xml/", // sitemapindex provides compressed sitemaps
+    "https://www.allrecipes.com/gsindex.xml", // sitemapindex provides compressed sitemaps 
+    "https://www.thepaleomom.com/sitemap_index.xml", // sitemapindex provides non-compressed sitemaps
+    "https://www.strictchef.com/sitemap.xml", // urlset
+    "https://www.ibeccreative.com/sitemap" // urlset
 ];
 
-// Sitemap Array
-// let sitemapArray = [
-//     "https://www.delish.com/sitemap_index.xml/",
-// ];
 
-// Get parent level XML sitemaps as an array of JSON objects
+
+let allUrls = [];
+
+// First, get parent level XML sitemaps as an array of JSON objects
 let parentSiteMaps = getSitemapsAsJson(sitemapArray);
 parentSiteMaps
 .then((result) => {
     console.log("Parent Site Maps: ", result);
+
+    // Add any parsed urlset(s) to the allUrls array
+    handleUrlsets(result.filter(item => {
+        if(typeof item["urlset"] !== "undefined") {
+            return true;
+        }
+    }));
+
+    
+    // Handle sitemapindex objects (under which more sitemaps are nested, potentially compressed)
+    handleSitemapindex(result.filter(item => {
+        if (typeof item["sitemapindex"] !== "undefined") {
+            return true;
+        }
+    }));
+
+
 })
 
 
 
+/**
+ * Handle urlsets
+ * @param {array} urlsetArray is an array of urlset objects parsed from xml
+ */
+function handleUrlsets(urlsetArray) {
+    // Push urls to allUrls array
+    urlsetArray.forEach(urlSet => {
+        urlSet["urlset"]["url"].forEach(url => {
+            allUrls.push(url["loc"][0])
+        })
+    })
+    // console.log("URLSET URLs: ", allUrls);
+}
 
+
+
+/**
+ * Handle sitemapindex Objects
+ * Gets the URL's of all sitemaps listed in the sitemapindex, and proceeds to get them as JSON using getSitemapsAsJson
+ * @param {array} sitemapindexArray is an array of sitemapindex objects parsed from xml
+ */
+function handleSitemapindex(sitemapindexArray) {
+    // console.log("Site Map Indexes: ", sitemapindexArray);
+    console.log("Site Map Index Example: ", sitemapindexArray[0]["sitemapindex"]["sitemap"]);
+}
 
 
 /**
@@ -59,6 +100,7 @@ function getSitemapsAsJson(sitemapArray) {
 
 
                 // If sitemap is a compressed file
+
 
 
             }));
